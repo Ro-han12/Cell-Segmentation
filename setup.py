7447108dfea0e4e776d9b97aac67687781e95070
rohan <rohannair2939@gmail.com>
2024-03-23 16:57:28 +0530

    requirements & setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup 
+setup(
+    name='cellSegmentation',
+    version='0.0.0',
+    author='Rohan',
+    author_email='rohannair2939@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
