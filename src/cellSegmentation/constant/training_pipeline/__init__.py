e00fdab36989abf5e2ab8cc2c01b04a3476b7271
rohan <rohannair2939@gmail.com>
2024-03-24 17:47:33 +0530

    model training done

--- src/cellSegmentation/constant/training_pipeline/__init__.py
+++ src/cellSegmentation/constant/training_pipeline/__init__.py
@@ -9,3 +9,9 @@ DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1yEjWa5ii-J9ofrawmKsPF
 DATA_VALIDATION_DIR_NAME: str='data_validation'
 DATA_VALIDATION_STATUS_FILE='status.txt'
 DATA_VALIDATION_ALL_REQUIRED_FILES=['train','valid','test','data.yaml']
+
+
+MODEL_TRAINER_DIR_NAME:str="model_trainer"
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME:str='yolov8s-seg.pt'
+MODEL_TRAINER_NO_EPOCHS:int=10
+MODEL_TRAINER_BATCH_SIZE:int=16
--- src/cellSegmentation/pipeline/training_pipeline.py
+++ src/cellSegmentation/pipeline/training_pipeline.py
@@ -3,16 +3,23 @@ from src.cellSegmentation.logger import logging
 from src.cellSegmentation.exception import AppException
 from src.cellSegmentation.components.data_ingestion import DataIngestion
 from src.cellSegmentation.components.data_validation import DataValidation
+from src.cellSegmentation.components.model_trainer import ModelTrainer
 
-from src.cellSegmentation.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
 
-from src.cellSegmentation.entity.artifacts_entity import (DataIngestionArtifact,DataValidationArtifact)
+from src.cellSegmentation.entity.config_entity import (DataIngestionConfig,
+                                                 DataValidationConfig,
+                                                 ModelTrainerConfig)
+
+from src.cellSegmentation.entity.artifacts_entity import (DataIngestionArtifact,
+                                                    DataValidationArtifact,
+                                                    ModelTrainerArtifact)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
 
     
@@ -38,7 +45,8 @@ class TrainPipeline:
         except Exception as e:
             raise AppException(e, sys)
         
-        
+
+
     
     def start_data_validation(
         self, data_ingestion_artifact: DataIngestionArtifact
@@ -63,11 +71,41 @@ class TrainPipeline:
 
         except Exception as e:
             raise AppException(e, sys) from e
-        
-    def run_pipeline(self)-> None:
+
+
+
+    
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
         try:
-            data_ingestion_artifact=self.start_data_ingestion()
-            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise AppException(e, sys)
+        
+        
+
+        
+
+    
+
+    def run_pipeline(self) -> None:
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
+
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
+            
+            else:
+                raise Exception("Your data is not in correct format")
+
         
         except Exception as e:
-            raise AppException(e,sys)
+            raise AppException(e, sys)
